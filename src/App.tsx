ccb8433c37be2b7093ff38590e29a4ed8ecd129b
Gabriel <gabriel.santiago@tqi.com.br>
2022-07-28 19:02:28 -0300

    improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import { Link } from 'react-router-dom';
 import Routes from 'routes/Routes';
 import ThemeProvider from 'common/providers/Theme';
+import MainLayout from 'components/Layouts/Main';
 
 const App = () => (
   <ThemeProvider>
-    <div id="app">
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/counter">Counter</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-
-      <main>
-        <Routes />
-      </main>
-
-      <footer>my footer</footer>
-    </div>
+    <MainLayout>
+      <Routes />
+    </MainLayout>
   </ThemeProvider>
 );
 
